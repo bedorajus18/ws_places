eec293ba2fd62cf716bf2f6677662ab01da6d54c
bedorajus18 <84348359+bedorajus18@users.noreply.github.com>
2021-08-26 14:41:10 +0200

    Usage de Postman
    Validator / add etc

--- src/Controller/PlaceController.php
+++ src/Controller/PlaceController.php
@@ -7,6 +7,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Exception\NotEncodableValueException;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use App\Entity\Place;
 
 class PlaceController extends AbstractController
 {
@@ -38,5 +44,37 @@ public function findById(PlaceRepository $placeRepository,$id,NormalizerInterfac
         'content-type' => 'application/json'
     ]);
     return $reponse;
+    }
+
+
+    
+    /**
+     * @Route("/api/place/", name="api_place_add", methods="POST")
+     */
+    public function add(EntityManagerInterface $entityManager, Request $request, SerializerInterface $serializer, ValidatorInterface $validator)
+    {
+        $contenu = $request->getContent();
+        /*
+        $place = $serializer->deserialize($contenu, Place::class, 'json');
+        $entityManager->persist($place);
+        $entityManager->flush();
+
+        return $this->json($place, 201, [], ['groups' => 'person:read']);
+    */
+        try{
+            $place = $serializer->deserialize($contenu, Place::class, 'json');
+            $errors = $validator->validate($place);
+            if (count($errors) > 0)
+            {
+                return $this->json($errors, 400);
+            }
+            $entityManager->persist($place);
+            $entityManager->flush();
+            return $this->json($place, 201, [], ['groups' => 'place:read']);
+        }
+        catch (NotEncodableValueException $e)
+        {
+            return $this->json(['status' => 400, 'message' => $e->getMessage()]);
+        }
     }
 }
